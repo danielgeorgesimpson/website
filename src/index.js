f16e8410bc57ad0967edb8f2c81d600e2c97cd3c
yaysee <80547411+yaysee@users.noreply.github.com>
2021-09-26 23:16:43 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+React.render(<h1>Test</h1>, document.getElementById('root'));
